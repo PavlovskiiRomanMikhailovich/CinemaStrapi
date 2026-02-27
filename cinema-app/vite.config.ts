c26b97d7a8162d65077b31f4ad51fc0df8950e53
PavlovskiiRomanMikhailovich <romanpavlovsk0808@gmail.com>
2026-02-27 10:17:55 +0300

    alias vriables

--- cinema-app/vite.config.ts
+++ cinema-app/vite.config.ts
@@ -4,26 +4,43 @@ import react from '@vitejs/plugin-react';
 
 const tsconfig = require('./tsconfig.json');
 
+const ROOT_PATH = path.resolve(__dirname);
 const SRC_PATH = path.resolve(__dirname, 'src');
 
 const parseTsConfigPaths = (paths: Record<string, string[]>): Record<string, string> => {
-  const webpackConfigAliases: Record<string, string> = {};
+  const aliases: Record<string, string> = {};
 
   Object.entries(paths).forEach(([alias, pathsArray]) => {
     const cleanAlias = alias.replace(/\/\*$/, '');
     const rawPath = pathsArray[0];
     const cleanPath = rawPath.replace(/\/\*$/, '');
-    const fullPath = path.join(SRC_PATH, cleanPath);
+    const fullPath = path.resolve(__dirname, cleanPath);
     
-    webpackConfigAliases[cleanAlias] = fullPath;
+    aliases[cleanAlias] = fullPath;
   });
 
-  return webpackConfigAliases;
+  return aliases;
 };
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: parseTsConfigPaths(tsconfig.compilerOptions.paths),
+    alias: parseTsConfigPaths(tsconfig.compilerOptions.paths)
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // @ts-ignore
+        importer: (url: string) => {
+          if (url.startsWith('@/')) {
+            const filePath = url.replace('@/', '');
+            const finalPath = filePath.endsWith('.scss') ? filePath : `${filePath}.scss`;
+            const fullPath = path.join(SRC_PATH, finalPath);
+            return { file: fullPath };
+          }
+          return null;
+        }
+      }
+    }
+  }
 });
